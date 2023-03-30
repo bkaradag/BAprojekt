21c63c9d4de65ec06820c3c2e5a6525a3158d263
Berkay Karadag <berkay.karadag@outlook.com>
2023-03-30 14:25:01 +0200

    grafiken hinzufügen

--- app.py
+++ app.py
@@ -81,94 +81,61 @@ def get_dashboard():
 def get_campuswetter():
     if request.method == 'POST':
 
-        datum, temperatur, rel_feuchte, wind_gesch, luft_druck, global_str, strahlug_bilanz, niederschlag_10m, niederschlag_tag, uv_index = get_campus_wetter_data()
-        #datum_ser,temp_ser = berechnen_campus_wetter_daten()
+        datum_ser, temp_ser, rel_feuchte_ser,wind_gesch_ser, luft_druck_ser, luft_druck_ser, global_st_ser, strahlug_bilanz_ser, niederschlag_10m_ser, niederschlag_tag_ser, uv_index_ser, m_mw_list, j_mw_list, zj_mw_list = berechnen_campus_wetter_daten()
 
-        # fig1 = go.Figure()
-        # fig1.add_trace(go.Line(x=datum_ser, y=temp_ser, name='Temperatur'))
-        # fig1.add_trace(go.Line(x=m_mw_list[1].index, y=m_mw_list[1].values, name='Aylık Ortalama'))
-        # fig1.add_trace(go.Line(x=j_mw_list[1].index, y=j_mw_list[1].values, name='Yıllık Ortalama'))
-        # fig1.add_trace(go.Line(x=j_mw_list[1].index, y=j_mw_list[1].values, name='10 Yıllık Ortalama'))
-        # fig1.update_layout(title='Temperatur', xaxis_title='Datum', yaxis_title='Temperatur')
-        # graf1JSON = json.dumps(fig1, cls=plotly.utils.PlotlyJSONEncoder)
+        # Plotly grafiklerini oluştur
+        fig1 = go.Figure()
+        fig1.add_trace(go.Line(x=datum_ser, y=temp_ser, name='Temperatur'))
+        fig1.add_trace(go.Line(x=m_mw_list[1].index, y=m_mw_list[1].values, name='Mittelwert Monat'))
+        fig1.add_trace(go.Line(x=j_mw_list[1].index, y=j_mw_list[1].values, name='Mittelwert Jahr'))
+        fig1.add_trace(go.Line(x=j_mw_list[1].index, y=j_mw_list[1].values, name='Mittelwert 10 Jahre'))
+        # Grafik ayarlarını yap
+        fig1.update_layout(title='Temperatur', xaxis_title='Datum', yaxis_title='Temperatur')
+        # Grafikleri HTML olarak döndür
+        graf1JSON = json.dumps(fig1, cls=plotly.utils.PlotlyJSONEncoder)
 
+        fig2 = go.Figure()
+        fig2.add_trace(go.Line(x=datum_ser, y=rel_feuchte_ser, name='rel_feuchte'))
+        fig2.update_layout(title='Relative Feuchte', xaxis_title='Datum', yaxis_title='Relative Feuchte')
+        graf2JSON = json.dumps(fig2, cls=plotly.utils.PlotlyJSONEncoder)
 
-        # # Plotly grafiklerini oluştur
-        # fig1 = go.Figure()
-        # fig1.add_trace(go.Line(x=datum_series, y=temperatur_series, name='Temperatur'))
-        # # Aylık Ortalama Çizgisi Ekleme
-        #
-        # fig1.add_trace(go.Line(x=monthly_avg.index, y=monthly_avg.values, name='Aylık Ortalama'))
-        #
-        # # Yıllık Ortalama Çizgisi Ekleme
-        # yearly_avg = temperatur_series.resample('Y').mean()
-        # fig1.add_trace(go.Line(x=yearly_avg.index, y=yearly_avg.values, name='Yıllık Ortalama'))
-        #
-        # # 10 Yıllık Ortalama Çizgisi Ekleme
-        # ten_year_avg = temperatur_series.resample('10Y').mean()
-        # fig1.add_trace(go.Line(x=ten_year_avg.index, y=ten_year_avg.values, name='10 Yıllık Ortalama'))
-        #
-        # fig1.update_layout(title='Temperatur', xaxis_title='Datum', yaxis_title='Temperatur')
-        # graf1JSON = json.dumps(fig1, cls=plotly.utils.PlotlyJSONEncoder)
-
-        # fig1.add_trace(
-        #     go.Line(x=[min(datum), max(datum)], y=[m_mw_temperatur, m_mw_temperatur], name='Mittelwert Monat'))
-        # fig1.add_trace(
-        #     go.Line(x=[min(datum), max(datum)], y=[j_mw_temperatur, j_mw_temperatur], name='Mittelwert 1 Jahr'))
-        # fig1.add_trace(
-        #     go.Line(x=[min(datum), max(datum)], y=[zj_mw_temperatur, zj_mw_temperatur], name='Mittelwert 10 Jahre'))
-
-        # fig2 = go.Figure()
-        # fig2.add_trace(go.Line(x=datum, y=rel_feuchte, name='rel_feuchte'))
-        # # fig1.add_trace(go.Line(x=[min(datum), max(datum)], y=m_mw_rel_feuchte, name='Mittelwert Monat'))
-        # # fig1.add_trace(go.Line(x=[min(datum), max(datum)], y=[j_mw_temperatur, j_mw_temperatur], name='Mittelwert 1 Jahr'))
-        # # fig1.add_trace(go.Line(x=[min(datum), max(datum)], y=[zj_mittelwert, zj_mittelwert], name='Mittelwert 10 Jahre'))
-        #
         fig3 = go.Figure()
-        fig3.add_trace(go.Line(x=datum, y=wind_gesch, name='wind_gesch'))
-        #
-        # fig4 = go.Figure()
-        # fig4.add_trace(go.Line(x=datum, y=luft_druck, name='luft_druck'))
-        #
-        # fig5 = go.Figure()
-        # fig5.add_trace(go.Line(x=datum, y=global_str, name='global_str'))
-        #
-        # fig6 = go.Figure()
-        # fig6.add_trace(go.Line(x=datum, y=strahlug_bilanz, name='strahlug_bilanz'))
-        #
-        # fig7 = go.Figure()
-        # fig7.add_trace(go.Line(x=datum, y=niederschlag_10m, name='niederschlag_10m'))
-        #
-        # fig8 = go.Figure()
-        # fig8.add_trace(go.Line(x=datum, y=niederschlag_tag, name='niederschlag_tag'))
-        #
-        # fig9 = go.Figure()
-        # fig9.add_trace(go.Line(x=datum, y=uv_index, name='uv_index'))
-
-    # Grafik ayarlarını yap
-
-        # fig2.update_layout(title='Relative Feuchte', xaxis_title='Datum', yaxis_title='Relative Feuchte')
+        fig3.add_trace(go.Line(x=datum_ser, y=wind_gesch_ser, name='wind_gesch'))
         fig3.update_layout(title='Wind Geschwindigkeit', xaxis_title='Datum', yaxis_title='Wind Geschwindigkeit')
-        # fig4.update_layout(title='Luftdruck', xaxis_title='Datum', yaxis_title='Luftdruck')
-        # fig5.update_layout(title='Globalstrahlung', xaxis_title='Datum', yaxis_title='Globalstrahlung')
-        # fig6.update_layout(title='Strahlung Bilanz', xaxis_title='Datum', yaxis_title='Strahlung Bilanz')
-        # fig7.update_layout(title='Niederschlag 10Min', xaxis_title='Datum', yaxis_title='Niederschlag 10Min')
-        # fig8.update_layout(title='Niederschalg Tag', xaxis_title='Datum', yaxis_title='Niederschalg Tag')
-        # fig9.update_layout(title='UV-Index', xaxis_title='Datum', yaxis_title='UV-Index')
-
-    # Grafikleri HTML olarak döndür
-
-        # graf2JSON = json.dumps(fig2, cls=plotly.utils.PlotlyJSONEncoder)
         graf3JSON = json.dumps(fig3, cls=plotly.utils.PlotlyJSONEncoder)
-        # graf4JSON = json.dumps(fig4, cls=plotly.utils.PlotlyJSONEncoder)
-        # graf5JSON = json.dumps(fig5, cls=plotly.utils.PlotlyJSONEncoder)
-        # graf6JSON = json.dumps(fig6, cls=plotly.utils.PlotlyJSONEncoder)
-        # graf7JSON = json.dumps(fig7, cls=plotly.utils.PlotlyJSONEncoder)
-        # graf8JSON = json.dumps(fig8, cls=plotly.utils.PlotlyJSONEncoder)
-        # graf9JSON = json.dumps(fig9, cls=plotly.utils.PlotlyJSONEncoder)
+
+        fig4 = go.Figure()
+        fig4.add_trace(go.Line(x=datum_ser, y=luft_druck_ser, name='luft_druck'))
+        fig4.update_layout(title='Luftdruck', xaxis_title='Datum', yaxis_title='Luftdruck')
+        graf4JSON = json.dumps(fig4, cls=plotly.utils.PlotlyJSONEncoder)
+
+        fig5 = go.Figure()
+        fig5.add_trace(go.Line(x=datum_ser, y=global_st_ser, name='global_str'))
+        fig5.update_layout(title='Globalstrahlung', xaxis_title='Datum', yaxis_title='Globalstrahlung')
+        graf5JSON = json.dumps(fig5, cls=plotly.utils.PlotlyJSONEncoder)
+
+        fig6 = go.Figure()
+        fig6.add_trace(go.Line(x=datum_ser, y=strahlug_bilanz_ser, name='strahlug_bilanz'))
+        fig6.update_layout(title='Strahlung Bilanz', xaxis_title='Datum', yaxis_title='Strahlung Bilanz')
+        graf6JSON = json.dumps(fig6, cls=plotly.utils.PlotlyJSONEncoder)
+
+        fig7 = go.Figure()
+        fig7.add_trace(go.Line(x=datum_ser, y=niederschlag_10m_ser, name='niederschlag_10m'))
+        fig7.update_layout(title='Niederschlag 10Min', xaxis_title='Datum', yaxis_title='Niederschlag 10Min')
+        graf7JSON = json.dumps(fig7, cls=plotly.utils.PlotlyJSONEncoder)
+
+        fig8 = go.Figure()
+        fig8.add_trace(go.Line(x=datum_ser, y=niederschlag_tag_ser, name='niederschlag_tag'))
+        fig8.update_layout(title='Niederschalg Tag', xaxis_title='Datum', yaxis_title='Niederschalg Tag')
+        graf8JSON = json.dumps(fig8, cls=plotly.utils.PlotlyJSONEncoder)
+
+        fig9 = go.Figure()
+        fig9.add_trace(go.Line(x=datum_ser, y=uv_index_ser, name='uv_index'))
+        fig9.update_layout(title='UV-Index', xaxis_title='Datum', yaxis_title='UV-Index')
+        graf9JSON = json.dumps(fig9, cls=plotly.utils.PlotlyJSONEncoder)
 
     # Şablonu render et
-        return render_template('campuswetterhoexter.html',graf3JSON=graf3JSON)
+        return render_template('campuswetterhoexter.html', graf1JSON=graf1JSON, graf2JSON=graf2JSON, graf3JSON=graf3JSON, graf4JSON=graf4JSON, graf5JSON=graf5JSON, graf6JSON=graf6JSON, graf7JSON=graf7JSON, graf8JSON=graf8JSON, graf9JSON=graf9JSON)
     else:
         # istek methodu GET ise, sayfayı görüntüle
         stations = get_stationen_bodenfeuchte()
--- berechnen.py
+++ berechnen.py
@@ -7,6 +7,14 @@ def berechnen_campus_wetter_daten():
 
     datum_ser = pd.Series(campus_data[0])
     temp_ser = pd.Series(campus_data[1])
+    rel_feuchte_ser = pd.Series(campus_data[2])
+    wind_gesch_ser = pd.Series(campus_data[3])
+    luft_druck_ser = pd.Series(campus_data[4])
+    global_st_ser = pd.Series(campus_data[5])
+    strahlug_bilanz_ser = pd.Series(campus_data[6])
+    niederschlag_10m_ser = pd.Series(campus_data[7])
+    niederschlag_tag_ser = pd.Series(campus_data[8])
+    uv_index_ser = pd.Series(campus_data[9])
 
     series = [pd.Series(campus_data[i], index=pd.to_datetime(datum_ser)) for i in range(1, len(campus_data))]
 
@@ -14,7 +22,7 @@ def berechnen_campus_wetter_daten():
     j_mw_list = [mw.resample('Y').mean() for mw in series]
     zj_mw_list = [mw.resample('10Y').mean() for mw in series]
 
-    return datum_ser,temp_ser
+    return datum_ser, temp_ser, rel_feuchte_ser,wind_gesch_ser, luft_druck_ser, luft_druck_ser, global_st_ser, strahlug_bilanz_ser, niederschlag_10m_ser, niederschlag_tag_ser, uv_index_ser, m_mw_list, j_mw_list, zj_mw_list
 
 
 
